5904cb15e1a194a68914bcfa8640aa065b855847
Barath97 <barath.kj97@gmail.com>
2024-02-12 16:31:47 +0530

    All packages and dependencies created

---
+++ ecom-shop/src/test/java/com/mvc/ecomshop/EcomShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.ecomshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
