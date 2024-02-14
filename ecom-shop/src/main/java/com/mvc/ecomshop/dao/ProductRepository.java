ccada42d280a05e2839f05c61650ecb8f1b40fdc
Barath97 <barath.kj97@gmail.com>
2024-02-14 11:41:25 +0530

    ALL CURD operation and Business logic completed

--- ecom-shop/src/main/java/com/mvc/ecomshop/dao/ProductRepository.java
+++ ecom-shop/src/main/java/com/mvc/ecomshop/dao/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.mvc.ecomshop.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.mvc.ecomshop.model.Products;
 @Repository
 public interface ProductRepository extends JpaRepository<Products, Integer> {
 
+	Products findByVariant(String variant);
+	Optional<Products> findByModel(String model);
 }
--- ecom-shop/src/main/java/com/mvc/ecomshop/service/ProductServiceImpl.java
+++ ecom-shop/src/main/java/com/mvc/ecomshop/service/ProductServiceImpl.java
@@ -1,7 +1,11 @@
 package com.mvc.ecomshop.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mvc.ecomshop.dao.ProductRepository;
 import com.mvc.ecomshop.model.Products;
 
 import jakarta.transaction.Transactional;
@@ -9,29 +13,76 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class ProductServiceImpl implements ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public Products createProduct(Products product) {
+		 String variant =product.getVariant();
 		 
-		return null;
+		 try {
+			 Products isExist= productRepository.findByVariant(variant);
+			 
+			 if(isExist == null) {
+				 return productRepository.save(product);
+			 }
+			 else {
+				 throw new Exception("Product Already Exists");
+			 }
+		 }catch(Exception e) {
+			 e.printStackTrace();
+			 return null;
+		 }
 	}
 
 	@Override
 	public Products updateProduct(Products product) {
 		 
-		return null;
+		 try {
+			 if(productRepository.findById(product.getId()).isPresent()) {
+				 return productRepository.save(product);
+			 }
+			 else {
+				 throw new Exception("product not found");
+			 }
+		 }catch(Exception e) {
+			 e.printStackTrace();
+			 return null;
+		 }
 	}
 
 	@Override
 	public Products findProductByModelName(String modelName) {
 		 
-		return null;
+		 try {
+			 Optional<Products> productsOptional = productRepository.findByModel(modelName);
+			 if(productsOptional.isPresent()) {
+				 return productsOptional.get();
+			 }
+			 else {
+				 throw new Exception("Model not exist");
+			 }
+		 }catch(Exception e) {
+			 throw new RuntimeException("Error occured during finding the product");
+		 }
 	}
 
 	@Override
-	public void deleteProductById(int id) {
-		 
-		
-	}
+    public void deleteProductById(int id) {
+
+        try{
+             Optional<Products> productsOptional = productRepository.findById(id);
+
+             if(productsOptional.isPresent()){
+                 productRepository.deleteById(id);
+            }else {
+                 throw new Exception("No product found with ID: " + id);
+            }
+        } catch (Exception e) {
+             throw new RuntimeException("Error while deleting product", e);
+        }
+
+    }
 
 }
